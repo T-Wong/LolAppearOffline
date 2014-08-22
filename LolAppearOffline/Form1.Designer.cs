cf425b946776b1d262c7f8f3660313eef890a190
Tyler Wong <ty-w@live.com>
2014-08-21 22:05:54 -0700

    Updated gui and started blocking implementation

--- LolAppearOffline/Form1.Designer.cs
+++ LolAppearOffline/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.disableButton = new System.Windows.Forms.Button();
+            this.regionComboBox = new System.Windows.Forms.ComboBox();
+            this.enableButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // disableButton
+            // 
+            this.disableButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.disableButton.Location = new System.Drawing.Point(221, 14);
+            this.disableButton.Name = "disableButton";
+            this.disableButton.Size = new System.Drawing.Size(75, 23);
+            this.disableButton.TabIndex = 0;
+            this.disableButton.Text = "Disable Chat";
+            this.disableButton.UseVisualStyleBackColor = true;
+            this.disableButton.Click += new System.EventHandler(this.disableButton_Click);
+            // 
+            // regionComboBox
+            // 
+            this.regionComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.regionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.regionComboBox.FormattingEnabled = true;
+            this.regionComboBox.Items.AddRange(new object[] {
+            "North America",
+            "EU West",
+            "EU Nordic & East",
+            "Brazil",
+            "Latin America North",
+            "Latin America South",
+            "Turkey",
+            "Oceania",
+            "Russia",
+            "Republic of Korea"});
+            this.regionComboBox.Location = new System.Drawing.Point(93, 14);
+            this.regionComboBox.Name = "regionComboBox";
+            this.regionComboBox.Size = new System.Drawing.Size(121, 21);
+            this.regionComboBox.TabIndex = 1;
+            // 
+            // enableButton
+            // 
+            this.enableButton.Location = new System.Drawing.Point(12, 12);
+            this.enableButton.Name = "enableButton";
+            this.enableButton.Size = new System.Drawing.Size(75, 23);
+            this.enableButton.TabIndex = 2;
+            this.enableButton.Text = "Enable Chat";
+            this.enableButton.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 42);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(283, 96);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(265, 141);
+            this.ClientSize = new System.Drawing.Size(308, 161);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.enableButton);
+            this.Controls.Add(this.regionComboBox);
+            this.Controls.Add(this.disableButton);
             this.Name = "Form1";
             this.Text = "LoL Appear Offline";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +104,11 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button disableButton;
+        private System.Windows.Forms.ComboBox regionComboBox;
+        private System.Windows.Forms.Button enableButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- LolAppearOffline/Form1.cs
+++ LolAppearOffline/Form1.cs
@@ -5,21 +5,56 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        private void addFirewall(String ip)
+        {
+            Type tNetFwPolicy2 = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+            INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(tNetFwPolicy2);
+            var currentProfiles = fwPolicy2.CurrentProfileTypes;
+
+            // Creates a new rule
+            INetFwRule2 inboundRule = (INetFwRule2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            inboundRule.Enabled = true;
+            inboundRule.LocalPorts = "1234";
+            inboundRule.Protocol = 6; // TCP
+            // ...
+            inboundRule.Profiles = currentProfiles;
+
+            // Add the rule
+            INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            firewallPolicy.Rules.Add(inboundRule);
+        }
         public Form1()
         {
+            // Dictionary used to map region with IP
+            Dictionary<string, string> regionsIP = new Dictionary<string, string>();
+            regionsIP.Add("North America", "216.133.234.21");
+            regionsIP.Add("EU West", "185.40.64.69");
+            regionsIP.Add("EU Nordic & East", "0");
+            regionsIP.Add("Brazil", "0");
+            regionsIP.Add("Latin America North", "0");
+            regionsIP.Add("Latin America South", "0");
+            regionsIP.Add("Turkey", "0");
+            regionsIP.Add("Oceania", "0");
+            regionsIP.Add("Russia", "0");
+            regionsIP.Add("Republic of Korea", "0");
+            
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        private void disableButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("test");
         }
     }
 }
